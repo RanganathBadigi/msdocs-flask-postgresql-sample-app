b57284f1541049bda2ced19bb36ab8d377d84f2c
RanganathBadigi <146017007+RanganathBadigi@users.noreply.github.com>
2025-05-12 19:27:17 +0530

    Update development.py

--- azureproject/development.py
+++ azureproject/development.py
@@ -1,10 +1,10 @@
 import os
 
-# DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-#     dbuser=os.environ['DBUSER'],
-#     dbpass=os.environ['DBPASS'],
-#     dbhost=os.environ['DBHOST'],
-#     dbname=os.environ['DBNAME']
-# )
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.environ['DBUSER'],
+    dbpass=os.environ['DBPASS'],
+    dbhost=os.environ['DBHOST'],
+    dbname=os.environ['DBNAME']
+)
 
-DATABASE_URI = os.getenv('DBConnectionString')
+#DATABASE_URI = os.getenv('DBConnectionString')
